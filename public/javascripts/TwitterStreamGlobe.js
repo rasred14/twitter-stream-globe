cba6cdb8ec3039fb04616008c5782dff66475211
rasred14 <rasred14@yahoo.gr>
2017-05-23 19:48:41 +0300

    Update TwitterStreamGlobe.js

--- public/javascripts/TwitterStreamGlobe.js
+++ public/javascripts/TwitterStreamGlobe.js
@@ -163,7 +163,7 @@
 	 */ 
 	function render () {
 
-		earthMesh.rotation.y = earthMesh.rotation.y + 0.005;
+		earthMesh.rotation.y = earthMesh.rotation.y + 0.015;
 		
 	  renderer.autoClear = false;
 	  renderer.clear();
